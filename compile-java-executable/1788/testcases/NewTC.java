150039d657c5c4c7544d947ca6765477e72efbdb
Kartik CoTestertest2 <km.test+3@testgrid.io>
2026-06-17 08:46:45 +0100

    Restored Version from Git Commit - Auto Commit

--- compile-java-executable/1788/testcases/NewTC.java
+++ compile-java-executable/1788/testcases/NewTC.java
@@ -1,5 +1,5 @@
-import bcd;
 import abc;
+import bcd;
 import io.testgrid.listeners.TestListener;
 import io.testgrid.listeners.RetryFailedTestCases;
 import io.testgrid.tg;
@@ -18,19 +18,16 @@ import io.appium.java_client.ios.IOSDriver;
 import org.testng.annotations.Test;
 
 @Listeners(TestListener.class);
-public class varr {
+public class newtc {
 
 	@Test(retryAnalyzer = RetryFailedTestCases.class)
-	public void varr() {
+	public void newtc() {
 		tg.openDevice();
-		tg.swipe(Direction.DOWN);
-		tg.wait("ele_EnteremailaddressEditText1781676776708", ComparisonType.IS_VISIBLE);
-		tg.type("ele_EnteremailaddressEditText1781676776708", "#TGITVAR.First", false);
-		tg.printLogs(var_Gstr);
-		tg.printLogs(var_ra_rint);
-				var_Gstr = tg.readFromAPI("Req_api.json").getString();
-		tg.printLogs(var_Gstr);
-		tg.wait(5);
+				tg.swipe(Direction.DOWN);
+				tg.wait("ele_SignUpTextView1781681796229", ComparisonType.IS_VISIBLE);
+				tg.click("ele_SignUpTextView1781681796229", 1);
+				tg.wait("ele_SignInTextView1781681802461", ComparisonType.IS_VISIBLE);
+				tg.click("ele_SignInTextView1781681802461", 1);
 		tg.close();
 	}
 }
