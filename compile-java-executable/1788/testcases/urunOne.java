d7f8cd120ee06a5c304b3408c16d3e37e94a7a90
Kartik CoTestertest2 <km.test+3@testgrid.io>
2026-06-17 08:22:41 +0100

    Initial Version Commit

---
+++ compile-java-executable/1788/testcases/urunOne.java
@@ -0,0 +1,40 @@
+import abc;
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import io.testgrid.enums.Alert;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class urunone {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void urunone() {
+		tg.openDevice();
+				tg.swipe(Direction.DOWN);
+				tg.wait("ele_EnteremailaddressEditText1781676776708", ComparisonType.IS_VISIBLE);
+				tg.click("ele_EnteremailaddressEditText1781676776708", 1);
+				tg.wait("ele_EnteremailaddressEditText1781676776708", ComparisonType.IS_VISIBLE);
+				tg.type("ele_EnteremailaddressEditText1781676776708", "mail", true);
+				tg.wait("ele_EnterpasswordEditText1781676786573", ComparisonType.IS_VISIBLE);
+				tg.click("ele_EnterpasswordEditText1781676786573", 1);
+				tg.wait("ele_EnterpasswordEditText1781676786573", ComparisonType.IS_VISIBLE);
+				tg.type("ele_EnterpasswordEditText1781676786573", "passss", true);
+				tg.wait("ele_textinputendiconImageButton1781676810574", ComparisonType.IS_VISIBLE);
+				tg.click("ele_textinputendiconImageButton1781676810574", 1);
+				tg.wait("ele_SignInButton1781676815494", ComparisonType.IS_VISIBLE);
+				tg.click("ele_SignInButton1781676815494", 1);
+		tg.close();
+	}
+}
---
+++ compile-java-executable/1788/testcases/varr.java
@@ -0,0 +1,34 @@
+import abc;
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import io.testgrid.enums.Alert;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class varr {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void varr() {
+		tg.openDevice();
+		tg.swipe(Direction.DOWN);
+		tg.wait("ele_EnteremailaddressEditText1781676776708", ComparisonType.IS_VISIBLE);
+		tg.type("ele_EnteremailaddressEditText1781676776708", "#TGITVAR.First", false);
+		tg.printLogs(var_Gstr);
+		tg.printLogs(var_ra_rint);
+				var_Gstr = tg.readFromAPI("Req_api.json").getString();
+		tg.printLogs(var_Gstr);
+		tg.close();
+	}
+}
