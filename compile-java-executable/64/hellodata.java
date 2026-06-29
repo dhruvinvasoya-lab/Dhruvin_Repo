2b121f9345eeb67363b8bbceafaa9dd160ddd048
Upendra Team user <upendra.goswami+7@testgrid.io>
2026-06-29 15:14:29 +0000

    Test Suite Added

---
+++ compile-java-executable/64/hellodata.java
@@ -0,0 +1,23 @@
+@Listeners(TestListener.class)
+public class hellodata {
+
+    public static String var_ra_TestOne = "Testjfejoifjijfjsaojfiosjfiojsaiofjiosajfiosjiofjsaiojfiosajfiojsaofjsaiojfiojsaiodjasiojdiosjdioajsiodjasodjsajdoijsaidjasiojdioasjd";
+
+    public static String var_neval = "adsasdfasdfa";
+
+    public static String var_urbundle = "com.app.urunner";
+
+    public static JSONObject var_upjson = new JSONObject();
+
+    public static JSONObject var_JSONG = new JSONObject();
+
+    public static Double var_ra_DBR = 60.60;
+
+    public static Double var_DBdata = 50.50;
+
+    public static int var_ra_TotalR = 60;
+
+    public static int var_Totaldata = 50;
+
+    public static String var_FullName = "upendra Goswami";
+}
