0db06d19eaed42b1e3da8b6497ba81447f786625
HashiCorp Vault <hashicorpvaulttest@gmail.com>
2026-07-03 18:13:36 +0530

    Commit check

--- compile-java-executable/42/testcases/tc03.java
+++ compile-java-executable/42/testcases/tc03.java
@@ -20,24 +20,13 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 @Listeners(TestListener.class);
-public class tc001 {
+public class tc03 {
 
 	@Test(retryAnalyzer = RetryFailedTestCases.class)
-	public void tc001() {
+	public void tc03() {
 		tg.openBrowser();
 		tg.wait(2);
-		tg.wait("ele_username603", ComparisonType.IS_VISIBLE);
-		tg.click("ele_username603", 1);
-		tg.wait("ele_username603", ComparisonType.IS_VISIBLE);
-		tg.type("ele_username603", "Vault_HashiCorp_password@@secret/data/testgrid");
-		tg.wait("ele_password527", ComparisonType.IS_VISIBLE);
-		tg.click("ele_password527", 1);
-		tg.wait("ele_password417", ComparisonType.IS_VISIBLE);
-		tg.click("ele_password417", 1);
-		tg.wait("ele_password417", ComparisonType.IS_VISIBLE);
-		tg.typeEncrypted("ele_password417", "Vault_HashiCorp_password@@secret/data/login");
-		tg.wait("ele_submit524", ComparisonType.IS_VISIBLE);
-		tg.click("ele_submit524", 1);
+		tg.typeEncrypted("ele_password417", "Vault_HashiCorp_password@@secret/data/govind");
 		tg.close();
 	}
 }
