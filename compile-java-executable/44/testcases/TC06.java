0ebdf379ad32acfa3534335f0f13e6daa1d6ae20
HashiCorp Vault <hashicorpvaulttest@gmail.com>
2026-07-07 19:50:32 +0530

    Initial Version Commit

---
+++ compile-java-executable/44/testcases/TC06.java
@@ -0,0 +1,34 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class tc06 {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void tc06() {
+		tg.openBrowser();
+		tg.wait(2);
+		tg.type("ele_username603", "Vault_HashiCorp_db_user@@secret/data/database");
+		tg.typeEncrypted("ele_password417", "Vault_HashiCorp_password@@secret/data/testgrid");
+		tg.takeFullScreenshot();
+		tg.close();
+	}
+}
