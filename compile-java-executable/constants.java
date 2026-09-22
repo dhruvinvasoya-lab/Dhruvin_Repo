d16730bf933ac497c8e9bac3c7464d671e4f70a3
Dhruvin vasoya <dhruvin.vasoya+5@testgrid.io>
2026-09-22 12:56:04 +0000

    Initial Version Commit

---
+++ compile-java-executable/constants.java
@@ -0,0 +1,9 @@
+public class constants {
+	public static int var_in1 = 10;
+	public static int var_in2 = 20;
+	public static String var_ans1 = "Demo";
+	public static String var_xyz1 = "123";
+	public static String var_date1 = "DATE";
+	public static int var_ra_var1 = 34;
+	public static String var_ra_str123 = "DhruvinVasoya";
+}
