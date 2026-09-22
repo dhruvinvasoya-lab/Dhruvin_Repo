5e80848ab38501af689972b52210736ceca95397
QA Demo <upendra.goswami+25@testgrid.io>
2026-09-22 09:24:39 +0000

    Govind : Commit

---
+++ compile-java-executable/180/testcases/TDPGovind.java
@@ -0,0 +1,31 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import io.testgrid.enums.Alert;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class tdpgovind {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void tdpgovind() {
+		tg.openDevice();
+		tg.wait(2);
+		tg_String var_Name = "#TGITVAR.name";
+		tg_String var_Email = "#TGITVAR.email";
+		tg.printLogs(var_Name);
+		tg.printLogs(var_Email);
+		tg.close();
+	}
+}
